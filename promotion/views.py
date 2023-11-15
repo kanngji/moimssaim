bdafa8db732720480e2e650e7b82dc87fb2c53ff
kangji <alleneffort1994@gmail.com>
2023-11-16 02:19:34 +0900

    promotion_list todo:id값 promotion_detail구현

--- promotion/views.py
+++ promotion/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view, renderer_classes
+from .models import Post
 # Create your views here.
 
 @api_view(['GET','POST'])
 def promotion_list(request):
-    return render(request,'promotion/promotion_list.html')
+    promotion_list = Post.objects.order_by('-create_date')
+    context = {'promotion_list':promotion_list}
+    return render(request,'promotion/promotion_list.html',context)
+
+
